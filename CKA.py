bd45ca15ab5a289879bdc0ca5f18a69570171641
Youssef Kashef <kashefy@ni.tu-berlin.de>
2025-04-29 14:54:06 +0200

    formatting, debug msgs, return nan if dividing by zero

--- CKA.py
+++ CKA.py
@@ -4,16 +4,17 @@
 import math
 import numpy as np
 
+
 class CKA(object):
     def __init__(self):
-        pass 
-    
+        pass
+
     def centering(self, K):
         n = K.shape[0]
         unit = np.ones([n, n])
         I = np.eye(n)
         H = I - unit / n
-        return np.dot(np.dot(H, K), H) 
+        return np.dot(np.dot(H, K), H)
 
     def rbf(self, X, sigma=None):
         GX = np.dot(X, X.T)
@@ -24,19 +25,24 @@ class CKA(object):
         KX *= - 0.5 / (sigma * sigma)
         KX = np.exp(KX)
         return KX
- 
+
     def kernel_HSIC(self, X, Y, sigma):
         return np.sum(self.centering(self.rbf(X, sigma)) * self.centering(self.rbf(Y, sigma)))
 
     def linear_HSIC(self, X, Y):
         L_X = X @ X.T
         L_Y = Y @ Y.T
+        print('LXcpu')
+        print(L_X)
         return np.sum(self.centering(L_X) * self.centering(L_Y))
 
     def linear_CKA(self, X, Y):
         hsic = self.linear_HSIC(X, Y)
         var1 = np.sqrt(self.linear_HSIC(X, X))
         var2 = np.sqrt(self.linear_HSIC(Y, Y))
+
+        if var1 == 0 or var2 == 0:
+            return np.nan
 
         return hsic / (var1 * var2)
 
@@ -46,4 +52,3 @@ class CKA(object):
         var2 = np.sqrt(self.kernel_HSIC(Y, Y, sigma))
 
         return hsic / (var1 * var2)
-
